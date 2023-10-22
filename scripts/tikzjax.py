5ae3a9d0217bec15c3924939ff0ca0364651229c
HanchaiN <85230240+HanchaiN@users.noreply.github.com>
2023-10-22 22:15:57 +0700

    fixup! build: migrate to mdBook

--- scripts/tikzjax.py
+++ scripts/tikzjax.py
@@ -6,10 +6,13 @@ def process_content(content):
     content = re.sub(r'````tikz([\s\S]*?)````', r'<script type="text/tikz">\1</script>', content, re.DOTALL)
     return content
 
-def process_chapter(chapter):
+def process_item(item):
+    if (not "content" in item):
+        return
+    chapter = item['Chapter']
     chapter['content'] = process_content(chapter['content']);
     for sub_item in chapter['sub_items']:
-        process_chapter(sub_item["Chapter"])
+        process_item(sub_item)
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
@@ -18,5 +21,5 @@ if __name__ == '__main__':
 
     context, book = json.load(sys.stdin)
     for section in book['sections']:
-        process_chapter(section['Chapter'])
+        process_item(section)
     print(json.dumps(book))
