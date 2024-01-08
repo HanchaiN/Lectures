37347b3e4e7115e5fd380f67611afdcad4c06120
HanchaiN <85230240+HanchaiN@users.noreply.github.com>
2024-01-08 11:10:53 +0700

    build: add smilesDrawer

---
+++ scripts/smiles-drawer.py
@@ -0,0 +1,32 @@
+import json
+import sys
+import re
+
+
+def process_content(content):
+    content = re.sub(
+        r"````(?:smiles|SMILES)([\s\S]*?)````",
+        r'<script type="text/smiles">\1</script>',
+        content,
+        re.DOTALL,
+    )
+    return content
+
+
+def process_item(item):
+    if not "Chapter" in item:
+        return
+    item["Chapter"]["content"] = process_content(item["Chapter"]["content"])
+    for sub_item in item["Chapter"]["sub_items"]:
+        process_item(sub_item)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "supports":
+            sys.exit(0)
+
+    context, book = json.load(sys.stdin)
+    for section in book["sections"]:
+        process_item(section)
+    print(json.dumps(book))
