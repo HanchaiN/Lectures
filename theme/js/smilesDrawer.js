b92268e6ebe0d45809cd569930ccaef4c8b57dab
Hanchai Nonprasart <85230240+HanchaiN@users.noreply.github.com>
2024-01-09 22:56:31 +0700

    Update smilesDrawer.js

--- theme/js/smilesDrawer.js
+++ theme/js/smilesDrawer.js
@@ -1,26 +1,30 @@
 let smilesDrawer = new SmilesDrawer.Drawer({ width: 250, height: 250 });
 function stringToHash(string) {
-
-		let hash = 0;
-
-		if (string.length == 0) return hash;
-
-		for (i = 0; i < string.length; i++) {
-			char = string.charCodeAt(i);
-			hash = ((hash << 5) - hash) + char;
-			hash = hash & hash;
-		}
-
-		return hash;
+	let hash = 0;
+	if (string.length == 0) return hash;
+	for (i = 0; i < string.length; i++) {
+		char = string.charCodeAt(i);
+		hash = ((hash << 5) - hash) + char;
+		hash = hash & hash;
 	}
+	return hash;
+}
 document.querySelectorAll('script[type="text/SMILES"]').forEach(function (smiles) {
-	let smilesText = smiles.innerHTML;
-	const canvasId = smiles.id ? smiles.id + '-canvas' : 'smiles-canvas-' + stringToHash(smilesText);
-	SmilesDrawer.parse(smilesText, function (tree) {
-		let canvas = document.createElement('canvas', { width: 250, height: 250 });
-		canvas.id = canvasId;
-		smiles.replaceWith(canvas, smiles);
-		console.log(smilesText, canvas);
-		smilesDrawer.draw(tree, canvasId);
+	smiles.innerHTML
+		.split("\n")
+		.filter(line => line.length !== "")
+		.forEach(function (smilesText, i) {
+		const canvasId = smiles.id ? smiles.id + '-canvas' + i : 'smiles-canvas-' + stringToHash(smilesText);
+		SmilesDrawer.parse(smilesText, function (tree) {
+			let canvas = document.createElement('canvas', { 
+				width: 250, 
+				height: 250,
+			});
+			canvas.id = canvasId;
+			smiles.replaceWith(canvas, smiles);
+			smilesDrawer.draw(tree, canvasId);
+		}, function (err) {
+			console.log(err, smilesText);
+		});
 	});
 });
