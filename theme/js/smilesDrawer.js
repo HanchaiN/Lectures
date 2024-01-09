2f024a3a32b9c425badf4f0d9cf693e94f5a9d00
Hanchai Nonprasart <85230240+HanchaiN@users.noreply.github.com>
2024-01-09 23:00:05 +0700

    Update smilesDrawer.js

--- theme/js/smilesDrawer.js
+++ theme/js/smilesDrawer.js
@@ -1,30 +1,32 @@
-let smilesDrawer = new SmilesDrawer.Drawer({ width: 250, height: 250 });
-function stringToHash(string) {
-	let hash = 0;
-	if (string.length == 0) return hash;
-	for (i = 0; i < string.length; i++) {
-		char = string.charCodeAt(i);
-		hash = ((hash << 5) - hash) + char;
-		hash = hash & hash;
+window.addEventListener("load", () => {
+	let smilesDrawer = new SmilesDrawer.Drawer({ width: 250, height: 250 });
+	function stringToHash(string) {
+		let hash = 0;
+		if (string.length == 0) return hash;
+		for (i = 0; i < string.length; i++) {
+			char = string.charCodeAt(i);
+			hash = ((hash << 5) - hash) + char;
+			hash = hash & hash;
+		}
+		return hash;
 	}
-	return hash;
-}
-document.querySelectorAll('script[type="text/SMILES"]').forEach(function (smiles) {
-	smiles.innerHTML
-		.split("\n")
-		.filter(line => line.length !== "")
-		.forEach(function (smilesText, i) {
-		const canvasId = smiles.id ? smiles.id + '-canvas' + i : 'smiles-canvas-' + stringToHash(smilesText);
-		SmilesDrawer.parse(smilesText, function (tree) {
-			let canvas = document.createElement('canvas', { 
-				width: 250, 
-				height: 250,
+	document.querySelectorAll('script[type="text/SMILES"]').forEach(function (smiles) {
+		smiles.innerHTML
+			.split("\n")
+			.filter(line => line.length !== "")
+			.forEach(function (smilesText, i) {
+			const canvasId = smiles.id ? smiles.id + '-canvas' + i : 'smiles-canvas-' + stringToHash(smilesText);
+			SmilesDrawer.parse(smilesText, function (tree) {
+				let canvas = document.createElement('canvas', { 
+					width: 250, 
+					height: 250,
+				});
+				canvas.id = canvasId;
+				smiles.replaceWith(canvas, smiles);
+				smilesDrawer.draw(tree, canvasId);
+			}, function (err) {
+				console.log(err, smilesText);
 			});
-			canvas.id = canvasId;
-			smiles.replaceWith(canvas, smiles);
-			smilesDrawer.draw(tree, canvasId);
-		}, function (err) {
-			console.log(err, smilesText);
 		});
 	});
 });
