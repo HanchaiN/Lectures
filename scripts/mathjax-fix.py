d7c27bfec0352057b5d3025d8dada2f203cb8ccf
Hanchai Nonprasart <85230240+HanchaiN@users.noreply.github.com>
2024-01-11 00:57:53 +0700

    Update mathjax-fix.py

--- scripts/mathjax-fix.py
+++ scripts/mathjax-fix.py
@@ -6,13 +6,13 @@ import re
 def process_content(content):
     content = re.sub(
         r"\$\$([\s\S]*?)(?<!(\\{2})*\\)\$\$",
-        r"\[\1\]",
+        r"\\[\1\\]",
         content,
         re.DOTALL,
     )
     content = re.sub(
         r"\$([\s\S]*?)(?<!(\\{2})*\\)\$",
-        r"\(\1\)",
+        r"\\(\1\\)",
         content,
         re.DOTALL,
     )
