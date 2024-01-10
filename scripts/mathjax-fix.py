6e661befa3ca29c44c7c601ff1ade824c0b26fcf
HanchaiN <85230240+HanchaiN@users.noreply.github.com>
2024-01-10 12:12:27 +0700

    Update GitHub Actions workflow and book configuration

--- scripts/mathjax-fix.py
+++ scripts/mathjax-fix.py
@@ -2,24 +2,29 @@ import json
 import sys
 import re
 
+
 def process_content(content):
-    content = re.sub(r'(?<!\\)\$\$([\s\S]*?)(?<!\\)\$\$', r'\\\\[\1\\\\]', content, re.DOTALL)
-    content = re.sub(r'(?<!\\)\$([\s\S]*?)(?<!\\)\$', r'\\\\(\1\\\\)', content, re.DOTALL)
+    content = re.sub(
+        r"(?<!\\)\\\[([\s\S]*?)(?<!\\)\\\]", r"\$\$\1\$\$", content, re.DOTALL
+    )
+    content = re.sub(r"(?<!\\)\\\(([\s\S]*?)(?<!\\)\\\)", r"\$\1\$", content, re.DOTALL)
     return content
 
+
 def process_item(item):
-    if (not "Chapter" in item):
+    if not "Chapter" in item:
         return
-    item['Chapter']['content'] = process_content(item['Chapter']['content']);
-    for sub_item in item['Chapter']['sub_items']:
+    item["Chapter"]["content"] = process_content(item["Chapter"]["content"])
+    for sub_item in item["Chapter"]["sub_items"]:
         process_item(sub_item)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     if len(sys.argv) > 1:
         if sys.argv[1] == "supports":
             sys.exit(0)
 
     context, book = json.load(sys.stdin)
-    for section in book['sections']:
+    for section in book["sections"]:
         process_item(section)
     print(json.dumps(book))
