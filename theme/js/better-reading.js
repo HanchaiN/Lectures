3d3ef7251d40a866637cfd40c8e5559f2c442506
Hanchai Nonprasart <85230240+HanchaiN@users.noreply.github.com>
2024-03-06 01:27:57 +0700

    Update better-reading.js

--- theme/js/better-reading.js
+++ theme/js/better-reading.js
@@ -1,7 +1,6 @@
 window.addEventListener("load", () => {
 	const splitter = new GraphemeSplitter();
 	const fixation_ratio = 0.4;
-
 	function processText(node, callback) {
 		if (node.nodeName === "SCRIPT") return;
 		switch (node.nodeType) {
@@ -15,21 +14,20 @@ window.addEventListener("load", () => {
 	}
 	const root = document.querySelector("main");
 	processText(root, function (textNode) {
-		let text = textNode.textContent;
-		let frag = document.createDocumentFragment();
-		text.split(/(\s*)/gmiu).forEach(function (word) {
+		const frag = document.createDocumentFragment();
+		textNode.textContent.split(/(\s*)/gmiu).forEach(function (word) {
 			if (word.trim() === "")
 				return frag.appendChild(document.createTextNode(word));
 			const charArray = splitter.splitGraphemes(word);
 			const fixationPoint = Math.ceil(charArray.length * fixation_ratio);
-			const word = document.createElement("span");
-			const fixp = document.createElement("span");
-			word.classList.add("better-reading");
-			fixp.classList.add("better-reading-fixation");
-			fixp.appendChild(document.createTextNode(charArray.slice(0, fixationPoint).join("")));
-			word.appendChild(fixp);
-			word.appendChild(document.createTextNode(charArray.slice(fixationPoint).join("")));
-			frag.appendChild(word);
+			const word_elm = document.createElement("span");
+			const fixation = document.createElement("span");
+			word_elm.classList.add("better-reading");
+			fixation.classList.add("better-reading-fixation");
+			fixation.appendChild(document.createTextNode(charArray.slice(0, fixationPoint).join("")));
+			word_elm.appendChild(fixation);
+			word_elm.appendChild(document.createTextNode(charArray.slice(fixationPoint).join("")));
+			frag.appendChild(word_elm);
 		});
 		textNode.replaceWith(frag);
 	});
