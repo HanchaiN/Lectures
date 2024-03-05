b0c00f8f4e772f9aa38d11adff0f5296b0866a54
Hanchai Nonprasart <85230240+HanchaiN@users.noreply.github.com>
2024-03-06 01:24:17 +0700

    Update better-reading.js

--- theme/js/better-reading.js
+++ theme/js/better-reading.js
@@ -1,22 +1,36 @@
-// TODO
 window.addEventListener("load", () => {
 	const splitter = new GraphemeSplitter();
 	const fixation_ratio = 0.4;
 
-	document.querySelector("main").classList.add("better-reading");
-	document.querySelectorAll("main _").forEach(function (element) {
-		let text = element.textContent ?? "";
-		let paragraphs = text.split(/[\n]/gmiu);
-		paragraphs.map(function (paragraph) {
-			let words = paragraph.split(/[ ]/gmiu);
-			words.map(function (word) {
-				if (word.trim() === "") return;
-				const charArray = splitter.splitGraphemes(word);
-				const fixationPoint = Math.ceil(charArray.length * fixation_ratio);
-				return `<span class="better-reading-fixation">${charArray.slice(0, fixationPoint).join("")}</span>${charArray.slice(fixationPoint).join("")}`;
-			});
-			return words.join(" ");
+	function processText(node, callback) {
+		if (node.nodeName === "SCRIPT") return;
+		switch (node.nodeType) {
+			case Node.ELEMENT_NODE:
+				node.childNodes.forEach((n) => processText(n, callback));
+				break;
+			case Node.TEXT_NODE:
+				callback(node);
+				break;
+		}
+	}
+	const root = document.querySelector("main");
+	processText(root, function (textNode) {
+		let text = textNode.textContent;
+		let frag = document.createDocumentFragment();
+		text.split(/(\s*)/gmiu).forEach(function (word) {
+			if (word.trim() === "")
+				return frag.appendChild(document.createTextNode(word));
+			const charArray = splitter.splitGraphemes(word);
+			const fixationPoint = Math.ceil(charArray.length * fixation_ratio);
+			const word = document.createElement("span");
+			const fixp = document.createElement("span");
+			word.classList.add("better-reading");
+			fixp.classList.add("better-reading-fixation");
+			fixp.appendChild(document.createTextNode(charArray.slice(0, fixationPoint).join("")));
+			word.appendChild(fixp);
+			word.appendChild(document.createTextNode(charArray.slice(fixationPoint).join("")));
+			frag.appendChild(word);
 		});
-		return paragraphs.join("\n");
+		textNode.replaceWith(frag);
 	});
 });
